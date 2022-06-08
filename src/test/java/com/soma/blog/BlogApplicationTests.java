e0e7974cbc26d2c714eab44e410eced89f745127
soma <363793011@qq.com>
2022-06-09 01:13:58 +0800

    init repo

---
+++ src/test/java/com/soma/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soma.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
